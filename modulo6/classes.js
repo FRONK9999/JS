6f933ab1085e7fcbdcbc3ec58fe2427a34a8d44a
Jhuan lucas <jhuanlucas12345@gmail.com>
2023-06-29 19:20:38 -0300

    Classes

---
+++ modulo6/classes.js
@@ -0,0 +1,24 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar() {
+        console.log(`${this.nome} está falando`);
+    }
+}
+
+
+//function constructor
+function Pessoa2(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function () {
+    console.log(`${this.nome} está falando`);
+}
+
+const p1 = new Pessoa('Jhuan', 'Lucas');
+const p2 = new Pessoa2('Jhuan', 'Lucas');
